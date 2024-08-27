8c73c8d3f6f7cd07b448dff423b00b5c02032561
Pawel4050 <paweubielinski@gmail.com>
2024-08-27 21:53:57 +0200

    up

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,4 @@
 const categoriesList = document.querySelector('#categories');
 const categoryItems = categoriesList.querySelectorAll('.item');
 console.log('Number fo categories:', categoryItems.length);
+console.log(categoryItems)
