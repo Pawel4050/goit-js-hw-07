2cdcc8a84436299cbbe2354fd62196cf827233b0
Pawel4050 <paweubielinski@gmail.com>
2024-08-28 14:09:52 +0200

    up

--- js/task-01.js
+++ js/task-01.js
@@ -3,10 +3,8 @@ const categoryItems = categoriesList.querySelectorAll('.item');
 console.log('Number fo categories:', categoryItems.length);
 
 categoryItems.forEach(category => {
-  const categoryHeader = category.querySelector('h2');
-
-  const categoryItems = category.querySelectorAll('li');
-
-  console.log(`Category: ${categoryHeader.textContent}`);
-  console.log(`Number of items in category: ${categoryItems.length}`);
+const categoryHeader = category.querySelector('h2');
+const innerItemsItems = category.querySelectorAll('li');
+console.log(`Category: ${categoryHeader.textContent}`);
+console.log(`Number of items in category: ${innerItems.length}`);
 });
