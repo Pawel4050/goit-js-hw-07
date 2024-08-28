f3e077efb609a959b3571bfd7b67e7ab6b1663c4
Pawel4050 <paweubielinski@gmail.com>
2024-08-28 14:26:11 +0200

    up

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,35 @@
-const categoriesList = document.querySelector('#categories');
-const categoryItems = categoriesList.querySelectorAll('.item');
-console.log('Number fo categories:', categoryItems.length);
+//const categoriesList = document.querySelector('#categories');
+//const categoryItems = categoriesList.querySelectorAll('.item');
+//console.log('Number fo categories:', categoryItems.length);
 
+//categoryItems.forEach(category => {
+//const categoryHeader = category.querySelector('h2');
+//const innerItems = category.querySelectorAll('li');
+//console.log(`Category: ${categoryHeader.textContent}`);
+//console.log(`Number of items in category: ${innerItems.length}`);
+//});
+
+
+// Wybieramy listę kategorii na podstawie jej ID
+const categoriesList = document.querySelector('#categories');
+
+// Wybieramy wszystkie elementy li.item
+const categoryItems = categoriesList.querySelectorAll('.item');
+
+// Wypisujemy liczbę kategorii
+console.log('Number of categories:', categoryItems.length);
+
+// Iterujemy przez każdą kategorię
 categoryItems.forEach(category => {
-const categoryHeader = category.querySelector('h2');
-const innerItems = category.querySelectorAll('li');
-console.log(`Category: ${categoryHeader.textContent}`);
-console.log(`Number of items in category: ${innerItems.length}`);
+  // Znajdujemy nagłówek <h2> dla danej kategorii
+  const categoryHeader = category.querySelector('h2');
+  
+  // Znajdujemy wszystkie elementy <li> wewnątrz danej kategorii
+  const innerItems = category.querySelectorAll('ul li');
+  
+  // Wypisujemy nazwę kategorii (tekst nagłówka <h2>)
+  console.log(`Category: ${categoryHeader.textContent}`);
+  
+  // Wypisujemy liczbę elementów w kategorii
+  console.log(`Number of items in category: ${innerItems.length}`);
 });
