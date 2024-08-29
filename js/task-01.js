9bdcc932f2c3eadd36b31231b467339aca836c34
Pawel4050 <paweubielinski@gmail.com>
2024-08-29 16:21:34 +0200

    up

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,13 @@
-//const categoriesList = document.querySelector('#categories');
-//const categoryItems = categoriesList.querySelectorAll('.item');
-//console.log('Number fo categories:', categoryItems.length);
+// const categoriesList = document.querySelector('#categories');
+// const categoryItems = categoriesList.querySelectorAll('.item');
+// console.log('Number fo categories:', categoryItems.length);
 
-//categoryItems.forEach(category => {
-//const categoryHeader = category.querySelector('h2');
-//const innerItems = category.querySelectorAll('li');
-//console.log(`Category: ${categoryHeader.textContent}`);
-//console.log(`Number of items in category: ${innerItems.length}`);
-//});
+// categoryItems.forEach(category => {
+// const categoryHeader = category.querySelector('h2');
+// const innerItems = category.querySelectorAll('li');
+// console.log(`Category: ${categoryHeader.textContent}`);
+// console.log(`Number of items in category: ${innerItems.length}`);
+// });
 
 
 // Wybieramy listę kategorii na podstawie jej ID
