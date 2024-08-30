05252714bf1a8777b291aeca78596fbfb0e1dcaf
Pawel4050 <paweubielinski@gmail.com>
2024-08-30 21:10:47 +0200

    up

--- js/task-03.js
+++ js/task-03.js
@@ -1,12 +1,8 @@
- // Pobranie elementów input i span z DOM
   const inputEl = document.getElementById("name-input");
   const outputEl = document.getElementById("name-output");
 
-  // Dodanie nasłuchiwania na zdarzenie 'input'
   inputEl.addEventListener("input", () => {
-    // Pobranie i przycięcie wartości z inputa
     const trimmedValue = inputEl.value.trim();
 
-    // Aktualizacja wartości w spanie lub ustawienie "Anonymous" gdy input jest pusty
     outputEl.textContent = trimmedValue ? trimmedValue : "Anonymous";
   });
